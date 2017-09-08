46861fb0c2fc54440c95d26930b969d0f536e371
Qudsia Ahmad <q4hmad@gmail.com>
2017-09-08 08:48:07 -0700

    boiler plate spec file and test file

---
+++ lib/anagram.rb
@@ -0,0 +1,8 @@
+!#/usr/bin/env ruby
+Class WordGame
+  def initialize()
+    @game = game
+  end
+
+  
+end
---
+++ spec/anagram_spec.rb
@@ -0,0 +1,39 @@
+require('rspec')
+require('WordGame')
+require('pry')
+
+describe ('#WordGame') do
+  it ('#WordGame') do
+  expect ( ('#WordGame')).to(eq('#WordGame'))
+  end
+end
+
+describe ('#WordGame') do
+  it ('#WordGame') do
+  expect ( ('#WordGame')).to(eq('#WordGame'))
+  end
+end
+
+describe ('#WordGame') do
+  it ('#WordGame') do
+  expect ( ('#WordGame')).to(eq('#WordGame'))
+  end
+end
+
+describe ('#WordGame') do
+  it ('#WordGame') do
+  expect ( ('#WordGame')).to(eq('#WordGame'))
+  end
+end
+
+describe ('#WordGame') do
+  it ('#WordGame') do
+  expect ( ('#WordGame')).to(eq('#WordGame'))
+  end
+end
+
+describe ('#WordGame') do
+  it ('#WordGame') do
+  expect ( ('#WordGame')).to(eq('#WordGame'))
+  end
+end
